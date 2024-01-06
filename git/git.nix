ed3133f5708518434502dc19dbcb2abe6f1fa001
D3faIt <8147434+D3faIt@users.noreply.github.com>
2024-01-06 09:30:59 +0100

    Git store works now??

--- git/git.nix
+++ git/git.nix
@@ -5,11 +5,34 @@
     enable = true;
     userName  = "D3faIt";
     userEmail = "8147434+D3faIt@users.noreply.github.com";
-    extraConfig = {
-      credential = {
-        credentialStore = "secretservice";
-        helper = "${ pkgs.git.override { withLibsecret = true; } }/bin/git-credential-libsecret";
+  };
+
+  # ???
+  #programs.git = {
+  #  enable = true;
+  #  package = [pkgs.gitFull];
+  #  config.credential.helper = "libsecret";
+  #};
+  programs.ssh = {
+    enable = true;
+
+    controlMaster = "auto";
+    controlPath = "~/.ssh/sockets/master-%r@%n:%p";
+    controlPersist = "10m";
+
+    matchBlocks = let
+      idFile = "~/.ssh/id_ed25519";
+    in {
+      #"gitlab.com" = {
+      #  user = "git";
+      #  identityFile = idFile;
+      #};
+      "github.com" = {
+        user = "git";
+        identityFile = idFile;
       };
     };
   };
+
+  programs.password-store.enable = true;
 }
