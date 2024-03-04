ad49da6a00a3b7ba18f4461dc14fd52b8f70f140
Aadniz <8147434+Aadniz@users.noreply.github.com>
2024-03-04 12:48:17 +0100

    Output isn't used

--- app/emacs/custom.el
+++ app/emacs/custom.el
@@ -7,10 +7,10 @@
   (when (and (eq major-mode 'org-mode)
              (string-prefix-p (expand-file-name "~/Documents/org") buffer-file-name))
     (message "Syncing org files with unison...")
-    (let ((output (with-output-to-string
-                    (with-current-buffer standard-output
-                      (unless (= 0 (call-process-shell-command "unison org-files -ui text"))
-                        (error "Error: SYNCING FAILED! please run \"unison org-files\" manually"))))))
+    (let ((_ (with-output-to-string
+               (with-current-buffer standard-output
+                 (unless (= 0 (call-process-shell-command "unison org-files -ui text"))
+                   (error "Error: SYNCING FAILED! please run \"unison org-files\" manually"))))))
       (message "Org files synced successfully"))))
 (add-hook 'after-save-hook #'sync-to-unison)
 (add-hook 'find-file-hook #'sync-to-unison)
