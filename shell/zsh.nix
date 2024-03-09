69cc1c1e733e67e858ab716c1ee0f0485bfeaef9
Aadniz <8147434+Aadniz@users.noreply.github.com>
2024-03-09 08:57:02 +0100

    Beautiful ZSH prompt :3

--- shell/zsh.nix
+++ shell/zsh.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, dotfilesDir, ... }:
+{ config, lib, pkgs, dotfilesDir, theme, ... }:
 let
   myAliases = import ./aliases.nix {inherit dotfilesDir;};
 in
@@ -10,23 +10,98 @@ in
 
   programs.zsh = {
     enable = true;
+    # My teacher said oh-my-zsh is bloated
+    oh-my-zsh.enable = true;
     shellAliases = myAliases;
-    #histSize = 10000;
-    #histFile = "${config.xdg.dataHome}/zsh/history";
-
-    zplug = {
-      enable = true;
-      plugins = [
-        { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
-        { name = "zsh-users/zsh-syntax-highlighting"; }
-        { name = "zsh-users/zsh-completions"; }
-        { name = "zsh-users/zsh-history-substring-search"; }
-        #{ name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
-        { name = "themes/agnoster"; tags = ["from:oh-my-zsh"]; }
-      ];
+    dotDir = dotfilesDir;
+    history = {
+      size = 10000;
+      save = 10000;
     };
+    plugins = [
+      {
+        # will source zsh-autosuggestions.plugin.zsh
+        name = "zsh-autosuggestions";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-autosuggestions";
+          rev = "v0.7.0";
+          sha256 = "sha256-KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
+       };
+      }
+      {
+        name = "zsh-syntax-highlighting";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-syntax-highlighting";
+          rev = "0.7.0";
+          sha256 = "sha256-eRTk0o35QbPB9kOIV0iDwd0j5P/yewFFISVS/iEfP2g=";
+        };
+      }
+      {
+        name = "bullet-train";
+        file = "bullet-train.zsh-theme";
+        src = pkgs.fetchFromGitHub {
+          owner = "caiogondim";
+          repo = "bullet-train.zsh";
+          rev = "d60f62c34b3d9253292eb8be81fb46fa65d8f048";
+          sha256 = "sha256-EsoCrKXmAfhSNFvUka+BglBDXM1npef4ddg7SVScxSs=";
+        };
+      }
+    ];
+
+    initExtraFirst = ''
+      export BULLETTRAIN_PROMPT_ORDER=(
+        status
+        custom
+        dir
+        screen
+        perl
+        ruby
+        virtualenv
+        aws
+        go
+        rust
+        elixir
+        git
+        hg
+        cmd_exec_time
+      )
+      export BULLETTRAIN_PROMPT_SEPARATE_LINE=false
+      export BULLETTRAIN_PROMPT_CHAR=""
+      export BULLETTRAIN_PROMPT_ADD_NEWLINE=false
+      export BULLETTRAIN_CUSTOM_BG=${theme.primary}
+      export BULLETTRAIN_CUSTOM_FG=${theme.foreground}
+      export BULLETTRAIN_CUSTOM_MSG=千屋
+      export BULLETTRAIN_DIR_BG=${theme.color8}
+      export BULLETTRAIN_DIR_FG=${theme.foreground}
+    '';
 
     initExtra = ''
+      bindkey  "^[[H"    beginning-of-line
+      bindkey  "^[[F"    end-of-line
+      bindkey  "^[[3~"   delete-char
+      bindkey  "^[[1;5C" forward-word
+      bindkey  "^[[1;5D" backward-word
+      bindkey  "^[[A"    history-beginning-search-backward
+      bindkey  "^[[B"    history-beginning-search-forward
+
+      autoload -U history-search-end #needed for -end
+      zle -N history-beginning-search-backward-end history-search-end
+      zle -N history-beginning-search-forward-end history-search-end
+
+      #when going up go up only beggining of curr word history
+      bindkey '\e[A' history-beginning-search-backward-end
+      bindkey '\e[B' history-beginning-search-forward-end
+      #ctrl + arrow forward/backward word
+      bindkey "^[[1;5C" forward-word
+      bindkey "^[[1;5D" backward-word
+      #alt + arrow forward/backward word
+      bindkey "^[[1;3C" forward-word
+      bindkey "^[[1;3D" backward-word
+      #alt + delete delete whole word
+      bindkey "\e\x7f" backward-kill-word
+
       eval "$(${pkgs.zoxide}/bin/zoxide init zsh)"
       alias cd=z
     '';
