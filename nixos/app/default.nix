3d8110b78649071e793c0f0077328f0469d00b2e
Aadniz <8147434+Aadniz@users.noreply.github.com>
2024-04-22 13:50:30 +0200

    Need git when having barebones system config only

--- nixos/app/default.nix
+++ nixos/app/default.nix
@@ -9,6 +9,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    git
     vlc
     firefox
     pywal
