e43a3c4d143987850a2cb1bedc1980243e4c9c57
Aadniz <8147434+Aadniz@users.noreply.github.com>
2024-06-19 11:42:57 +0200

    Issue with Jetbrains and SSH tunnel with control p

--- ssh/default.nix
+++ ssh/default.nix
@@ -8,9 +8,9 @@
   programs.ssh = {
     enable = true;
 
-    controlMaster = "auto";
-    controlPath = "${config.home.homeDirectory}/.ssh/sockets/master-%r@%n:%p";
-    controlPersist = "10m";
+    #controlMaster = "auto";
+    #controlPath = "${config.home.homeDirectory}/.ssh/sockets/master-%r@%n:%p";
+    #controlPersist = "10m";
 
     matchBlocks = let
       idFile = "~/.ssh/id_ed25519";
