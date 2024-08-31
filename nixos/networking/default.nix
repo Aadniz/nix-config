5f607494b884673dacb0815b701c54b21b6934c0
Aadniz <8147434+Aadniz@users.noreply.github.com>
2024-09-01 00:36:15 +0200

    Now using br0 bridge

--- nixos/networking/default.nix
+++ nixos/networking/default.nix
@@ -10,29 +10,33 @@
     tcpdump
   ];
 
-  networking.hostName = "nix"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking = {
+    hostName = "nix"; # Define your hostname.
+    # Pick only one of the below networking options.
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  # Enable networking
-  networking.networkmanager.enable = false;
+    # Enable networking
+    networkmanager.enable = false;
 
-  # Disable firewall
-  networking.firewall.enable = false;
+    # Disable firewall
+    firewall.enable = false;
 
-  # Setup use of br0 instead of raw interface
-  networking.useDHCP = false;
-  networking.interfaces."enp38s0" = {
+    # Setup use of br0 instead of raw interface
     useDHCP = false;
-    ipv4.addresses = [{
-      address = "192.168.1.111";
-      prefixLength = 24;
-    }];
+    bridges."br0" = {
+      interfaces = [ "enp38s0" ];
+    };
+    interfaces = {
+      "br0".useDHCP = false;
+      "br0".ipv4.addresses = [ {
+        address = "192.168.1.111";
+        prefixLength = 24;
+      } ];
+      "enp39s0".useDHCP = false;
+      "enp38s0".useDHCP = false;
+    };
+    defaultGateway = "192.168.1.1";
+    nameservers = [ "9.9.9.9" "95.215.19.53" ];
   };
-  networking.interfaces."enp39s0" = {
-    useDHCP = false;
-  };
-  networking.defaultGateway = "192.168.1.1";
-  networking.nameservers = [ "9.9.9.9" "95.215.19.53" ];
 }
