931524c7e0e1521d83c56fe1ffee55c3362530c0
Aadniz <8147434+Aadniz@users.noreply.github.com>
2024-09-01 00:48:05 +0200

    Fixed i18n.inputMethod.enabled warning

--- misc/fcitx5.nix
+++ misc/fcitx5.nix
@@ -16,7 +16,7 @@ in {
   #};
 
   i18n.inputMethod = {
-    enabled = "fcitx5";
+    type = "fcitx5";
     fcitx5.addons = with pkgs; [
         fcitx5-mozc
         fcitx5-gtk
--- nixos/misc/ime.nix
+++ nixos/misc/ime.nix
@@ -21,7 +21,7 @@
       "ja_JP.UTF-8/UTF-8"
     ];
     inputMethod = {
-      enabled = "fcitx5";
+      type = "fcitx5";
       fcitx5.addons = with pkgs; [fcitx5-mozc];
     };
   };
