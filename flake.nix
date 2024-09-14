a3f582f1ef813c26192a7ea367dc1c8c416f359d
Aadniz <8147434+Aadniz@users.noreply.github.com>
2024-09-14 19:08:41 +0200

    initial commit for v2

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+    sops-nix = {
+      url = "github:mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+
+    # Sushi machine
+    nixosConfigurations.sushi = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/sushi/configuration.nix
+        home-manager.nixosModules.home-manager
+      ];
+    };
+
+  };
+}
