1e0bdf5364f0a17530a020955b987d69d16e924a
Aadniz <8147434+Aadniz@users.noreply.github.com>
2024-10-26 02:21:42 +0200

    Tuned locales

--- hosts/sushi/configuration.nix
+++ hosts/sushi/configuration.nix
@@ -41,10 +41,10 @@
     LC_MEASUREMENT = "de_DE.UTF-8";
     LC_MONETARY = "de_DE.UTF-8";
     LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
     LC_PAPER = "de_DE.UTF-8";
     LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
+    LC_TIME = "ja_JP.UTF-8";
   };
 
   # Enable the X11 windowing system.
