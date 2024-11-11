13ec8c4b6ae037da465b3b33fd2aa89d1831b1e8
Aadniz <8147434+Aadniz@users.noreply.github.com>
2024-11-11 14:55:42 +0100

    Flake update, including fixes

--- common/networking.nix
+++ common/networking.nix
@@ -2,4 +2,7 @@
 
 {
   networking.nameservers = [ "95.215.19.53" "9.9.9.9" ];
+
+  # Prevent "warning: the following units failed: NetworkManager-wait-online.service"
+  systemd.services.NetworkManager-wait-online.enable = false;
 }
--- modules/apps/discord.nix
+++ modules/apps/discord.nix
@@ -1,38 +1,43 @@
 {config, lib, pkgs, inputs, ... }:
 
 {
-  hm.imports = [
-    inputs.nixcord.homeManagerModules.nixcord
+  environment.systemPackages = with pkgs; [
+    vesktop
   ];
 
-  hm.programs.nixcord = {
-    enable = true;
-    # disable discord (enabled by default)
-    discord = {
-      enable = false;
-      vencord.enable = false;
-      openASAR.enable = false;
-    };
-    # use vesktop instead (wayland optimized discord client)
-    vesktop = {
-      enable = true;
-      package = pkgs.vesktop;
-    };
-    config.plugins = {
-      callTimer.enable = true;
-      crashHandler.enable = true;
-      fakeNitro.enable = true;
-      friendsSince.enable = true;
-      notificationVolume.enable = true;
-      volumeBooster.enable = true;
-      webScreenShareFixes.enable = true;
-      spotifyCrack.enable = true;
-      typingTweaks.enable = true;
-      USRBG.enable = true;
-      oneko.enable = true;
-      moreKaomoji.enable = true;
-      anonymiseFileNames.enable = true;
-      noBlockedMessages.enable = true;
-    };
-  };
+  ## Doesn't work
+  ## hm.imports = [
+  ##   inputs.nixcord.homeManagerModules.nixcord
+  ## ];
+
+  ## hm.programs.nixcord = {
+  ##   enable = true;
+  ##   # disable discord (enabled by default)
+  ##   discord = {
+  ##     enable = false;
+  ##     vencord.enable = false;
+  ##     openASAR.enable = false;
+  ##   };
+  ##   # use vesktop instead (wayland optimized discord client)
+  ##   vesktop = {
+  ##     enable = true;
+  ##     package = pkgs.vesktop;
+  ##   };
+  ##   config.plugins = {
+  ##     callTimer.enable = true;
+  ##     crashHandler.enable = true;
+  ##     fakeNitro.enable = true;
+  ##     friendsSince.enable = true;
+  ##     notificationVolume.enable = true;
+  ##     volumeBooster.enable = true;
+  ##     webScreenShareFixes.enable = true;
+  ##     spotifyCrack.enable = true;
+  ##     typingTweaks.enable = true;
+  ##     USRBG.enable = true;
+  ##     oneko.enable = true;
+  ##     moreKaomoji.enable = true;
+  ##     anonymiseFileNames.enable = true;
+  ##     noBlockedMessages.enable = true;
+  ##   };
+  ## };
 }
