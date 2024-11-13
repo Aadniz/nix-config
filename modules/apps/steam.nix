7577cba8e2d5e5ab3116697198f13e489b31fc62
Aadniz <8147434+Aadniz@users.noreply.github.com>
2024-11-14 00:33:38 +0100

    Steam worked after adding this

--- modules/apps/steam.nix
+++ modules/apps/steam.nix
@@ -4,4 +4,13 @@
   environment.systemPackages = with pkgs; [
    steam
   ];
+
+  hardware.opengl = {
+    ## radv: an open-source Vulkan driver from freedesktop
+    driSupport32Bit = true;
+
+    ## amdvlk: an open-source Vulkan driver from AMD
+    extraPackages = [ pkgs.amdvlk ];
+    extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
+  };
 }
