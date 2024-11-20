bb46963e1361e85bfa8a54be51c50ea86b150bd0
Aadniz <8147434+Aadniz@users.noreply.github.com>
2024-11-20 23:09:08 +0100

    Added nicotine+ and vlc

--- hosts/sushi/settings.nix
+++ hosts/sushi/settings.nix
@@ -46,7 +46,7 @@
   # Don't need any additional config for the apps, just throw them in here
   environment.systemPackages = with pkgs; [
     element-desktop anki gparted krita signal-desktop zoom-us userhosts
-    bruno kdePackages.elisa
+    bruno kdePackages.elisa nicotine-plus vlc
   ];
 
   username = "chiya";
