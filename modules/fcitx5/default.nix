34fb69958f6da64f88f387da425a5769864d6102
Aadniz <8147434+Aadniz@users.noreply.github.com>
2024-12-16 21:31:52 +0100

    Now autostart fcitx5 with computer

--- modules/fcitx5/default.nix
+++ modules/fcitx5/default.nix
@@ -18,4 +18,19 @@
       waylandFrontend = true;
     };
   };
+
+  hm.wayland.windowManager.sway.config.startup = [ { command = "fcitx5"; } ];
+
+  # This is dangerous! It's a lazy workaround to delete files
+  # after auto-writes to them from fcitx5's exit routines.
+  # Scripts are explicitly not supposed to be called like this
+  # according to the Home Manager manual, but I'm doing it it
+  # anyway because it's more convenient. Also I'm lazy.
+  hm.home.activation = {
+    "delete-existing-fcitx5-files" = {
+      before = [ "checkLinkTargets" ];
+      after = [ ];
+      data = "rm $VERBOSE_ARG -rf $HOME/.config/fcitx5/* ~/.local/share/fcitx5/*";
+    };
+  };
 }
