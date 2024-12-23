d5c810f06280c03870fc5e2d2361c2e2c2abd84d
Aadniz <8147434+Aadniz@users.noreply.github.com>
2024-12-23 13:05:03 +0100

    Now using swapped ESC <-> CAPS for laptop keyboard

--- modules/wms/sway/hardware.nix
+++ modules/wms/sway/hardware.nix
@@ -7,9 +7,11 @@
         xkb_layout = "us";
         xkb_variant = "altgr-intl";
         xkb_numlock = "enabled";
-        xkb_options = "grp:win_space_toggle";
         repeat_delay = "200";
         repeat_rate = "50";
+      };
+      "1:1:AT_Translated_Set_2_keyboard" = {
+        xkb_options = "caps:swapescape";
       };
       "5426:123:Razer_Razer_Viper_Ultimate_Dongle" = {
         dwt = "enabled";
