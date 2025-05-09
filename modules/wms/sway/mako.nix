74a865bee002019d9400fc91738942c4dd019f74
Aadniz <8147434+Aadniz@users.noreply.github.com>
2025-05-10 01:47:01 +0200

    Flake update

--- modules/wms/sway/mako.nix
+++ modules/wms/sway/mako.nix
@@ -9,28 +9,30 @@ in
   environment.systemPackages = with pkgs; [ mako ];
   hm.services.mako = {
     enable = true;
-    backgroundColor = bg;
-    progressColor = "over ${hi}FF";
-    borderSize = 0;
-    width = 400;
-    height = 300;
-    padding = "16";
-    #margin = "60,30";
-    defaultTimeout = 7000;
-    font = "Monospace 12";
-    layer = "overlay";
-    sort = "+time";
-    maxVisible = 6;
-    textColor = fg;
-    extraConfig = /* ini */ ''
-      [urgency=high]
-      border-color=${urgent}
-      border-size=3
-      default-timeout=0
-      [urgency=critical]
-      border-color=${urgent}
-      border-size=3
-      default-timeout=0
-    '';
+    settings = {
+      background-color = bg;
+      progress-color = "over ${hi}FF";
+      border-size = 0;
+      width = 400;
+      height = 300;
+      padding = "16";
+      #margin = "60,30";
+      default-timeout = 7000;
+      font = "Monospace 12";
+      layer = "overlay";
+      sort = "+time";
+      max-visible = 6;
+      text-color = fg;
+      #extraConfig = /* ini */ ''
+      #  [urgency=high]
+      #  border-color=${urgent}
+      #  border-size=3
+      #  default-timeout=0
+      #  [urgency=critical]
+      #  border-color=${urgent}
+      #  border-size=3
+      #  default-timeout=0
+      #'';
+    };
   };
 }
